6fed658c7d6f24ffae395a473fd9d557e0517b64
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2023-11-11 14:53:18 +0530

    setup starter files

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,15 @@
-import { url } from "./constants";
+import { experiences } from "./experiences";
+import { navLinks } from "./navLinks";
+import { projects } from "./projects";
+import { services } from "./services";
+import { technologies } from "./technologies";
+import { testimonials } from "./testimonials";
 
-export { url };
+export {
+  experiences,
+  navLinks,
+  projects,
+  services,
+  technologies,
+  testimonials,
+};
---
+++ src/constants/services.ts
@@ -0,0 +1,20 @@
+import { backend, creator, mobile, web } from "../../public/assets";
+
+export const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Native Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Content Creator",
+    icon: creator,
+  },
+];
